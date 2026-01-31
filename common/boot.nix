61bda26906728a50a9810b7e954dd450e5eeb522
Main user <daniloany@proton.me>
2026-01-31 12:19:08 +0100

    added wallpapers to limine and a wallpapers folder to modules

--- common/boot.nix
+++ common/boot.nix
@@ -1,10 +1,15 @@
-{pkgs, inputs, config, ... }:
+{pkgs, inputs, config, mod, ... }:
 {
   boot.loader = {
     limine ={
         enable = true;
         maxGenerations = 5;
         efiSupport = true;
+        style = {
+          wallpapers = [
+            "${mod}/wallpapers/wallhaven-o5ky29.png"
+          ];
+        };
     };
   };
 }
--- hosts/Artemis/configuration.nix
+++ hosts/Artemis/configuration.nix
@@ -5,14 +5,14 @@
       ./hardware-configuration.nix
 			./devices.nix
       "${mod}/GPU.nix"
-      "${mod}./hypr.nix"
-		  "${mod}./gnome.nix"
-	    "${mod}./dev.nix"
-	    "${mod}./gaming.nix"
-		  "${mod}./creativity.nix"
-	    "${mod}./wine.nix"
-		  "${mod}./audio.nix"
-		  "${mod}./remote-desktop.nix"
+      "${mod}/hypr.nix"
+		  "${mod}/gnome.nix"
+	    "${mod}/dev.nix"
+	    "${mod}/gaming.nix"
+		  "${mod}/creativity.nix"
+	    "${mod}/wine.nix"
+		  "${mod}/audio.nix"
+		  "${mod}/remote-desktop.nix"
     ];
 
   networking.hostName = "Artemis";
